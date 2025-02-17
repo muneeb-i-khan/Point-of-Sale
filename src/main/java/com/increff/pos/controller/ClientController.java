3b13f90bef75698bcc171a812775f146a7ab00d9
Muneeb Ilahi <muneebk015@gmail.com>
2025-02-17 14:28:14 +0530

    ADD: DTO Layer for client

--- src/main/java/com/increff/pos/controller/ClientController.java
+++ src/main/java/com/increff/pos/controller/ClientController.java
@@ -3,59 +3,48 @@ package com.increff.pos.controller;
 import com.increff.pos.dto.ClientDto;
 import com.increff.pos.model.ClientData;
 import com.increff.pos.model.ClientForm;
-import com.increff.pos.db.pojo.ClientPojo;
 import com.increff.pos.service.ApiException;
 import com.increff.pos.service.ClientService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Api
 @RestController
 public class ClientController {
     @Autowired
-    ClientService clientService;
+    ClientDto clientDto;
+
     @ApiOperation(value = "Post a client")
     @RequestMapping(path="/api/client/", method = RequestMethod.POST)
-    public void addClient(@RequestBody ClientForm ClientForm) {
-        ClientPojo p = ClientDto.convert(ClientForm);
-        clientService.addClient(p);
+    public void addClient(@RequestBody ClientForm clientForm) {
+        clientDto.addClient(clientForm);
     }
+
     @ApiOperation(value = "Delete a client")
     @RequestMapping(path="/api/client/{id}", method = RequestMethod.DELETE)
     public void delete(@PathVariable Long id) throws ApiException  {
-        clientService.getCheck(id);
-        clientService.deleteClient(id);
+        clientDto.deleteClient(id);
     }
+
     @ApiOperation(value = "Get a client based on it's Id")
     @RequestMapping(path = "/api/client/{id}", method = RequestMethod.GET)
     public ClientData get(@PathVariable Long id) throws ApiException {
-        clientService.getCheck(id);
-        ClientPojo p = clientService.getClient(id);
-        return ClientDto.convert(p);
+        return clientDto.getClient(id);
     }
 
     @ApiOperation(value = "Get all clients")
     @RequestMapping(path = "/api/client", method = RequestMethod.GET)
     public List<ClientData> getAll() {
-        List<ClientPojo> list = clientService.getAllClients();
-        List<ClientData> list2 = new ArrayList<ClientData>();
-        for(ClientPojo p : list) {
-            list2.add(ClientDto.convert(p));
-        }
-        return list2;
+        return clientDto.getAllClients();
     }
 
     @ApiOperation(value = "Update an existing client")
     @RequestMapping(path = "/api/client/{id}", method = RequestMethod.PUT)
-    public void update(@PathVariable Long id, @RequestBody ClientForm ClientForm) throws ApiException{
-        ClientPojo p = ClientDto.convert(ClientForm);
-        clientService.getCheck(id);
-        clientService.updateClient(id,p);
+    public void update(@PathVariable Long id, @RequestBody ClientForm clientForm) throws ApiException{
+        clientDto.updateClient(clientForm, id);
     }
 
 }
--- src/main/java/com/increff/pos/dto/ClientDto.java
+++ src/main/java/com/increff/pos/dto/ClientDto.java
@@ -3,19 +3,61 @@ package com.increff.pos.dto;
 import com.increff.pos.db.pojo.ClientPojo;
 import com.increff.pos.model.ClientData;
 import com.increff.pos.model.ClientForm;
+import com.increff.pos.service.ApiException;
+import com.increff.pos.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class ClientDto {
-    public static ClientData convert(ClientPojo p) {
+    @Autowired
+    ClientService clientService;
+
+    public ClientData convert(ClientPojo p) {
         ClientData clientData = new ClientData();
         clientData.setName(p.getName());
         clientData.setDescription(p.getDescription());
         clientData.setId(p.getId());
         return clientData;
     }
-    public static ClientPojo convert(ClientForm ClientForm) {
+    public ClientPojo convert(ClientForm ClientForm) {
         ClientPojo p = new ClientPojo();
         p.setName(ClientForm.getName());
         p.setDescription(ClientForm.getDescription());
         return p;
+    }
+
+    public void addClient(ClientForm clientForm) {
+        ClientPojo clientPojo = convert(clientForm);
+        clientService.addClient(clientPojo);
+    }
+
+    public void deleteClient(Long id) throws ApiException {
+        clientService.getCheck(id);
+        clientService.deleteClient(id);
+    }
+
+    public ClientData getClient(Long id) throws ApiException {
+        clientService.getCheck(id);
+        ClientPojo p = clientService.getClient(id);
+        return convert(p);
+    }
+
+    public List<ClientData> getAllClients() {
+        List<ClientPojo> list = clientService.getAllClients();
+        List<ClientData> list2 = new ArrayList<ClientData>();
+        for(ClientPojo p : list) {
+            list2.add(convert(p));
+        }
+        return list2;
+    }
+
+    public void updateClient(ClientForm clientForm, Long id) throws  ApiException{
+        ClientPojo p = convert(clientForm);
+        clientService.getCheck(id);
+        clientService.updateClient(id,p);
     }
 }
