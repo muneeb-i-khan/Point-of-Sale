4f8e5190238349e03b6cd87862df15e1f26d008a
Muneeb Ilahi <muneebk015@gmail.com>
2025-02-17 14:51:53 +0530

    ADD: DTO Layer for Inventory

--- src/main/java/com/increff/pos/controller/InventoryController.java
+++ src/main/java/com/increff/pos/controller/InventoryController.java
@@ -1,86 +1,56 @@
 package com.increff.pos.controller;
 
-import com.increff.pos.db.pojo.InventoryPojo;
-import com.increff.pos.db.pojo.ProductPojo;
+import com.increff.pos.dto.InventoryDto;
 import com.increff.pos.model.InventoryData;
 import com.increff.pos.model.InventoryForm;
 import com.increff.pos.service.ApiException;
-import com.increff.pos.service.InventoryService;
-import com.increff.pos.service.ProductService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Api
 @RestController
 public class InventoryController {
     @Autowired
-    InventoryService inventoryService;
-    @Autowired
-    ProductService productService;
+    InventoryDto inventoryDto;
 
     @ApiOperation(value = "Post an inventory")
     @RequestMapping(path="/api/inventory/", method = RequestMethod.POST)
-    public void add(@RequestBody InventoryForm inventoryForm) throws ApiException {
-        InventoryPojo inventoryPojo = convert(inventoryForm);
-        inventoryService.addInventory(inventoryPojo);
+    public void addInventory(@RequestBody InventoryForm inventoryForm) throws ApiException {
+        inventoryDto.addInventory(inventoryForm);
     }
 
     @ApiOperation(value = "Get all inventories")
     @RequestMapping(path = "/api/inventory", method = RequestMethod.GET)
     public List<InventoryData> getAll() {
-        List<InventoryPojo> list = inventoryService.getAllInventories();
-        List<InventoryData> list2 = new ArrayList<InventoryData>();
-        for(InventoryPojo p : list) {
-            list2.add(convert(p));
-        }
-        return list2;
+
     }
 
     @ApiOperation(value = "Get inventory")
     @RequestMapping(path = "/api/inventory/{id}", method = RequestMethod.GET)
     public InventoryData get(@PathVariable  Long id) throws ApiException {
-        inventoryService.getCheck(id);
-        InventoryPojo inventoryPojo = inventoryService.getInventory(id);
-        return convert(inventoryPojo);
+        return inventoryDto.getInventory(id);
+    }
+
+    @ApiOperation(value = "Get inventory based on barcode")
+    @RequestMapping(path = "/api/inventory/{barcode}", method = RequestMethod.GET)
+    public InventoryData get(@PathVariable  String barcode) throws ApiException {
+       return  inventoryDto.getInventory(barcode);
     }
 
     @ApiOperation(value = "Update an existing inventory")
     @RequestMapping(path = "/api/inventory/{id}", method = RequestMethod.PUT)
-    public void update(@PathVariable Long id, @RequestBody InventoryForm inventoryForm) throws ApiException{
-        InventoryPojo inventoryPojo = convert(inventoryForm);
-        inventoryService.getCheck(id);
-        inventoryService.updateInventory(id,inventoryPojo);
+    public void updateInventory(@PathVariable Long id, @RequestBody InventoryForm inventoryForm) throws ApiException{
+        inventoryDto.updateInventory(inventoryForm, id);
     }
 
     @ApiOperation(value = "Delete a inventory")
     @RequestMapping(path="/api/inventory/{id}", method = RequestMethod.DELETE)
-    public void delete(@PathVariable Long id) throws ApiException {
-        inventoryService.getCheck(id);
-        inventoryService.deleteInventory(id);
+    public void deleteInventory(@PathVariable Long id) throws ApiException {
+        inventoryDto.deleteInventory(id);
     }
 
-    public static InventoryData convert(InventoryPojo inventoryPojo) {
-        InventoryData inventoryData = new InventoryData();
-        inventoryData.setId(inventoryPojo.getId());
-        inventoryData.setBarcode(inventoryPojo.getBarcode());
-        inventoryData.setQuantity(inventoryPojo.getQuantity());
-        inventoryData.setProdName(inventoryPojo.getProductPojo().getName());
-        inventoryData.setProdId(inventoryPojo.getProductPojo().getId());
-        inventoryData.setClientName(inventoryPojo.getProductPojo().getClientPojo().getName());
-        return inventoryData;
-    }
-
-    public InventoryPojo convert(InventoryForm inventoryForm) throws ApiException {
-        InventoryPojo inventoryPojo = new InventoryPojo();
-        inventoryPojo.setBarcode(inventoryForm.getBarcode());
-        inventoryPojo.setQuantity(inventoryForm.getQuantity());
-        ProductPojo productPojo = productService.getProductByBarcode(inventoryForm.getBarcode());
-        inventoryPojo.setProductPojo(productPojo);
-        return inventoryPojo;
-    }
 }
--- src/main/java/com/increff/pos/dto/InventoryDto.java
+++ src/main/java/com/increff/pos/dto/InventoryDto.java
@@ -5,12 +5,78 @@ import com.increff.pos.db.pojo.ProductPojo;
 import com.increff.pos.model.InventoryData;
 import com.increff.pos.model.InventoryForm;
 import com.increff.pos.service.ApiException;
+import com.increff.pos.service.InventoryService;
 import com.increff.pos.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class InventoryDto {
     @Autowired
-    static ProductService productService;
+    ProductService productService;
 
+    @Autowired
+    InventoryService inventoryService;
+
+    public void addInventory(InventoryForm inventoryForm) throws ApiException {
+        InventoryPojo inventoryPojo = convert(inventoryForm);
+        inventoryService.addInventory(inventoryPojo);
+    }
+
+    public List<InventoryData> getAllInventories() {
+        List<InventoryPojo> list = inventoryService.getAllInventories();
+        List<InventoryData> list2 = new ArrayList<InventoryData>();
+        for(InventoryPojo p : list) {
+            list2.add(convert(p));
+        }
+        return list2;
+    }
+
+    public InventoryData getInventory(Long id) throws ApiException{
+        inventoryService.getCheck(id);
+        InventoryPojo inventoryPojo = inventoryService.getInventory(id);
+        return convert(inventoryPojo);
+    }
+
+    public InventoryData getInventory(String barcode) throws ApiException{
+        ProductPojo productPojo = productService.getProductByBarcode(barcode);
+        inventoryService.getCheck(productPojo.getId());
+        InventoryPojo inventoryPojo = inventoryService.getInventory(productPojo.getId());
+        return convert(inventoryPojo);
+    }
+
+    public void updateInventory(InventoryForm inventoryForm, Long id) throws ApiException {
+        InventoryPojo inventoryPojo = convert(inventoryForm);
+        inventoryService.getCheck(id);
+        inventoryService.updateInventory(id,inventoryPojo);
+    }
+
+    public void deleteInventory(Long id) throws ApiException {
+        inventoryService.getCheck(id);
+        inventoryService.deleteInventory(id);
+    }
+
+    public InventoryData convert(InventoryPojo inventoryPojo) {
+        InventoryData inventoryData = new InventoryData();
+        inventoryData.setId(inventoryPojo.getId());
+        inventoryData.setBarcode(inventoryPojo.getBarcode());
+        inventoryData.setQuantity(inventoryPojo.getQuantity());
+        inventoryData.setProdName(inventoryPojo.getProductPojo().getName());
+        inventoryData.setProdId(inventoryPojo.getProductPojo().getId());
+        inventoryData.setClientName(inventoryPojo.getProductPojo().getClientPojo().getName());
+        return inventoryData;
+    }
+
+    public InventoryPojo convert(InventoryForm inventoryForm) throws ApiException {
+        InventoryPojo inventoryPojo = new InventoryPojo();
+        inventoryPojo.setBarcode(inventoryForm.getBarcode());
+        inventoryPojo.setQuantity(inventoryForm.getQuantity());
+        ProductPojo productPojo = productService.getProductByBarcode(inventoryForm.getBarcode());
+        inventoryPojo.setProductPojo(productPojo);
+        return inventoryPojo;
+    }
 
 }
