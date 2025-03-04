5fbc5ddb8f284c74fa277083a3e2ca784993c6f7
Muneeb Illahi <muneebk015@gmail.com>
2025-03-04 08:27:07 +0530

    FIX: Fix return types

--- src/main/java/com/increff/pos/controller/InventoryController.java
+++ src/main/java/com/increff/pos/controller/InventoryController.java
@@ -26,23 +26,20 @@ public class InventoryController {
     private InventoryDto inventoryDto;
 
     @PostMapping
-    public ResponseEntity<Map<String, String>> addInventory(@Valid @RequestBody InventoryForm inventoryForm) throws ApiException {
+    public void addInventory(@Valid @RequestBody InventoryForm inventoryForm) throws ApiException {
         inventoryDto.addInventory(inventoryForm);
-        return ResponseEntity.ok(Collections.singletonMap("message", "Inventory added successfully"));
     }
 
     @ApiOperation(value = "Get all inventories")
     @GetMapping
-    public ResponseEntity<List<InventoryData>> getAll() throws ApiException {
-        List<InventoryData> inventories = inventoryDto.getAllInventories();
-        return ResponseEntity.ok(inventories);
+    public List<InventoryData> getAll() throws ApiException {
+        return inventoryDto.getAllInventories();
     }
 
     @ApiOperation(value = "Get inventory")
     @GetMapping("/{id}")
-    public ResponseEntity<InventoryData> get(@PathVariable Long id) throws ApiException {
-        InventoryData inventory = inventoryDto.getInventory(id);
-        return ResponseEntity.ok(inventory);
+    public InventoryData get(@PathVariable Long id) throws ApiException {
+        return inventoryDto.getInventory(id);
     }
 
     @ApiOperation(value = "Get all inventories with pagination")
@@ -56,20 +53,17 @@ public class InventoryController {
 
     @ApiOperation(value = "Get inventory based on barcode")
     @GetMapping("/barcode/{barcode}")
-    public ResponseEntity<InventoryData> getByBarcode(@PathVariable String barcode) throws ApiException {
-        InventoryData inventory = inventoryDto.getInventory(barcode);
-        return ResponseEntity.ok(inventory);
+    public InventoryData getByBarcode(@PathVariable String barcode) throws ApiException {
+        return inventoryDto.getInventory(barcode);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Map<String, String>> updateInventory(@PathVariable Long id, @RequestBody InventoryForm inventoryForm) throws ApiException {
+    public void updateInventory(@PathVariable Long id, @RequestBody InventoryForm inventoryForm) throws ApiException {
         inventoryDto.updateInventory(inventoryForm, id);
-        return ResponseEntity.ok(Collections.singletonMap("message", "Inventory updated successfully"));
     }
 
     @PostMapping("/upload")
-    public ResponseEntity<Map<String, String>> uploadInventory(@RequestParam("file") MultipartFile file) throws IOException, ApiException {
+    public void uploadInventory(@RequestParam("file") MultipartFile file) throws IOException, ApiException {
         inventoryDto.uploadInventory(file);
-        return ResponseEntity.ok(Collections.singletonMap("message", "Inventory file uploaded successfully"));
     }
 }
