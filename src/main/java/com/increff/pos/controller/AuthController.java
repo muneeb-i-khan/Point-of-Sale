23eee0343ee88938eea5a7e3e77cef196040181c
Muneeb Illahi <muneebk015@gmail.com>
2025-03-09 10:21:48 +0530

    FIX: Auth Controller return types

--- src/main/java/com/increff/pos/controller/AuthController.java
+++ src/main/java/com/increff/pos/controller/AuthController.java
@@ -6,59 +6,43 @@ import com.increff.pos.service.AuthService;
 import com.increff.pos.util.ApiException;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+
     @Autowired
     private AuthService authService;
 
     @ApiOperation(value = "Login")
     @PostMapping("/login")
-    public ResponseEntity<UserData> login(@RequestBody UserForm loginRequest, HttpSession session) {
-        try {
-            UserData userData = authService.login(loginRequest.getEmail(), loginRequest.getPassword(), session);
-            return ResponseEntity.ok(userData);
-        } catch (ApiException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
-        }
+    public UserData login(@RequestBody UserForm loginRequest, HttpSession session) throws ApiException {
+        return authService.login(loginRequest.getEmail(), loginRequest.getPassword(), session);
     }
 
     @ApiOperation(value = "Check session")
     @GetMapping("/check")
-    public ResponseEntity<UserData> checkSession(HttpSession session) {
-        try {
-            UserData userData = authService.getSessionUser(session);
-            return ResponseEntity.ok(userData);
-        } catch (ApiException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
-        }
+    public UserData checkSession(HttpSession session) throws ApiException {
+        return authService.getSessionUser(session);
     }
 
     @ApiOperation(value = "Logout")
     @PostMapping("/logout")
-    public ResponseEntity<Void> logout(HttpServletRequest request) {
+    public void logout(HttpServletRequest request) {
         HttpSession session = request.getSession(false);
         if (session != null) {
             session.invalidate();
         }
-        return ResponseEntity.ok().build();
     }
 
     @ApiOperation(value = "Sign up")
     @PostMapping("/signup")
-    public ResponseEntity<Void> signup(@Valid @RequestBody UserForm signupRequest) {
-        try {
-            authService.registerUser(signupRequest.getEmail(), signupRequest.getPassword());
-            return ResponseEntity.ok().build();
-        } catch (ApiException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
+    public void signup(@Valid @RequestBody UserForm signupRequest) throws ApiException {
+        authService.registerUser(signupRequest.getEmail(), signupRequest.getPassword());
     }
 }
