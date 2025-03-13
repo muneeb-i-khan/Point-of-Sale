50c310879eb4b460c95fb708279447f55f4f5410
Muneeb Illahi <muneebk015@gmail.com>
2025-03-13 08:57:11 +0530

    REFACTOR: DaysalereportService refactored

--- src/main/java/com/increff/pos/service/DaySaleReportService.java
+++ src/main/java/com/increff/pos/service/DaySaleReportService.java
@@ -1,9 +1,7 @@
 package com.increff.pos.service;
 
 import com.increff.pos.db.dao.DaySaleReportDao;
-import com.increff.pos.db.dao.OrderDao;
 import com.increff.pos.db.pojo.DaySaleReportPojo;
-import com.increff.pos.db.pojo.InventoryPojo;
 import com.increff.pos.flow.DaySaleReportFlow;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,38 +19,12 @@ public class DaySaleReportService {
     private DaySaleReportFlow daySaleReportFlow;
 
     @Transactional
-    //TODO: to refactor the code
     public void recordDailySales() {
         ZonedDateTime yesterday = ZonedDateTime.now().minusDays(1);
 
-        int orderCount = daySaleReportFlow.getOrderCountByDate(yesterday);
-        int itemSoldCount = daySaleReportFlow.getItemSoldCount(yesterday);
-        double revenue = daySaleReportFlow.getRevenue(yesterday);
-
-        //TODO: dont use syste,out, use logging instead
-        System.out.println("Generating Report for " + yesterday);
-        System.out.println("Orders: " + orderCount + ", Items Sold: " + itemSoldCount + ", Revenue: " + revenue);
-
-        if (orderCount > 0 || itemSoldCount > 0 || revenue > 0) {
-            DaySaleReportPojo existingReport = daySaleReportDao.findByDate(yesterday);
-
-            if (existingReport != null) {
-                existingReport.setOrderCount(orderCount);
-                existingReport.setItemSoldCount(itemSoldCount);
-                existingReport.setRevenue(revenue);
-                daySaleReportDao.update(existingReport);
-                System.out.println("Updated existing report for " + yesterday);
-            } else {
-                DaySaleReportPojo report = new DaySaleReportPojo();
-                report.setDate(yesterday);
-                report.setOrderCount(orderCount);
-                report.setItemSoldCount(itemSoldCount);
-                report.setRevenue(revenue);
-                daySaleReportDao.save(report);
-                System.out.println("Report saved for " + yesterday);
-            }
-        } else {
-            System.out.println("No sales data for " + yesterday + ", skipping report.");
+        DaySaleReportPojo report = generateReport(yesterday);
+        if (report != null) {
+            saveOrUpdateReport(report);
         }
     }
 
@@ -68,8 +40,37 @@ public class DaySaleReportService {
         return daySaleReportDao.countDaySaleReportPojo();
     }
 
-
     public List<DaySaleReportPojo> getReport() {
         return daySaleReportDao.selectAll();
     }
+
+    private DaySaleReportPojo generateReport(ZonedDateTime date) {
+        int orderCount = daySaleReportFlow.getOrderCountByDate(date);
+        int itemSoldCount = daySaleReportFlow.getItemSoldCount(date);
+        double revenue = daySaleReportFlow.getRevenue(date);
+
+        if (orderCount == 0 && itemSoldCount == 0 && revenue == 0) {
+            return null;
+        }
+
+        DaySaleReportPojo report = new DaySaleReportPojo();
+        report.setDate(date);
+        report.setOrderCount(orderCount);
+        report.setItemSoldCount(itemSoldCount);
+        report.setRevenue(revenue);
+        return report;
+    }
+
+    private void saveOrUpdateReport(DaySaleReportPojo report) {
+        DaySaleReportPojo existingReport = daySaleReportDao.findByDate(report.getDate());
+        if (existingReport != null) {
+            existingReport.setOrderCount(report.getOrderCount());
+            existingReport.setItemSoldCount(report.getItemSoldCount());
+            existingReport.setRevenue(report.getRevenue());
+            daySaleReportDao.update(existingReport);
+        } else {
+            daySaleReportDao.save(report);
+        }
+    }
+
 }
